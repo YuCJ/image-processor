1367a6fa8fee740a8782a6ba01aaaf3c12c81d47
yucj <nakts0123@gmail.com>
2024-02-28 21:52:56 +0800

    feat: add inputAccept

---
+++ src/useImageInput.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const defaultInputAccept = 'image/*';
+
+export default function useImageInput({ onChange, inputAccept }) {
+  const [imageFile, setImageFile] = useState(null);
+
+  const handleFileChange = event => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const reader = new window.FileReader();
+    reader.onload = event => {
+      setImageFile(event.target.result);
+    };
+    reader.readAsDataURL(file);
+
+    if (typeof onChange === 'function') {
+      onChange();
+    }
+  };
+
+  const input = (
+    <input
+      type="file"
+      accept={inputAccept ?? defaultInputAccept}
+      onChange={handleFileChange}
+    />
+  );
+
+  return [input, imageFile];
+}
