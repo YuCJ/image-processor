74eff6225e28a9575308b13b39e354514241ea2f
yucj <nakts0123@gmail.com>
2024-02-29 02:49:49 +0800

    feat: use konva to render canvas

--- src/cut-image-in-the-middle/Editing.js
+++ src/cut-image-in-the-middle/Editing.js
@@ -1,24 +1,44 @@
 import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Konva from 'konva';
 
-const Container = styled.div`
-  text-align: center;
-`;
+const Container = styled.div``;
 
 function Editing({ imageDataUrl, cuts, sortedCutIds }) {
-  const canvasRef = useRef(null);
+  const stageRef = useRef(null);
+  const konvaRef = useRef({});
+
+  // initialize
   useEffect(() => {
-    if (imageDataUrl) {
+    if (stageRef.current) {
+      const stage = new Konva.Stage({
+        container: stageRef.current,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+      const imageLayer = new Konva.Layer();
+      const cutsLayer = new Konva.Layer();
+
+      stage.add(imageLayer, cutsLayer);
+
+      konvaRef.current.stage = stage;
+      konvaRef.current.imageLayer = imageLayer;
+      konvaRef.current.cutsLayer = cutsLayer;
+    }
+  }, []);
+
+  // image
+  useEffect(() => {
+    if (imageDataUrl && konvaRef.current.imageLayer) {
       const image = new window.Image();
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
       image.onload = () => {
-        const width = image.width;
-        const height = image.height;
-        canvas.width = width;
-        canvas.height = height;
-        ctx.drawImage(image, 0, 0);
+        konvaRef.current.image = new Konva.Image({
+          image,
+          width: image.width,
+          height: image.height,
+        });
+        konvaRef.current.imageLayer.add(konvaRef.current.image);
       };
       image.src = imageDataUrl;
     }
@@ -30,7 +50,7 @@ function Editing({ imageDataUrl, cuts, sortedCutIds }) {
 
   return (
     <Container>
-      <canvas ref={canvasRef}></canvas>
+      <div ref={stageRef}></div>
       <code>{JSON.stringify(sortedCutIds)}</code>
       <code>{JSON.stringify(cuts)}</code>
     </Container>
@@ -39,7 +59,7 @@ function Editing({ imageDataUrl, cuts, sortedCutIds }) {
 
 Editing.propTypes = {
   imageDataUrl: PropTypes.string,
-  cuts: PropTypes.arrayOf(
+  cuts: PropTypes.objectOf(
     PropTypes.shape({
       top: PropTypes.number.isRequired,
       bottom: PropTypes.number.isRequired,
