a19f2370a7d0852fdd70115d3e4a55333d8d01b8
yucj <nakts0123@gmail.com>
2024-02-28 22:23:29 +0800

    feat: render image file into canvas

--- src/cut-image-in-the-middle/CutImageInTheMiddle.js
+++ src/cut-image-in-the-middle/CutImageInTheMiddle.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
+import useImageInput from '../useImageInput';
 
 const H1 = styled.h1`
   color: red;
 `;
 
 export default function CutImageInTheMiddle() {
-  return <H1>CutImageInTheMiddle</H1>;
+  const canvasRef = useRef(null);
+  const drawImage = ({ imageDataUrl }) => {
+    if (!imageDataUrl) return;
+
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+
+    const image = new window.Image();
+    image.onload = () => {
+      const width = image.width;
+      const height = image.height;
+      canvas.width = width;
+      canvas.height = height;
+      ctx.drawImage(image, 0, 0);
+    };
+    image.src = imageDataUrl;
+  };
+
+  const [input] = useImageInput({ onChange: drawImage });
+
+  return (
+    <div>
+      <H1>CutImageInTheMiddle</H1>
+      {input}
+      <canvas ref={canvasRef}></canvas>
+    </div>
+  );
 }
--- src/useImageInput.js
+++ src/useImageInput.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 const defaultInputAccept = 'image/*';
 
-export default function useImageInput({ onChange, inputAccept }) {
+export default function useImageInput({ onChange, inputAccept } = {}) {
   const [imageFile, setImageFile] = useState(null);
 
   const handleFileChange = event => {
@@ -12,12 +12,11 @@ export default function useImageInput({ onChange, inputAccept }) {
     const reader = new window.FileReader();
     reader.onload = event => {
       setImageFile(event.target.result);
+      if (typeof onChange === 'function') {
+        onChange({ imageDataUrl: event.target.result });
+      }
     };
     reader.readAsDataURL(file);
-
-    if (typeof onChange === 'function') {
-      onChange();
-    }
   };
 
   const input = (
